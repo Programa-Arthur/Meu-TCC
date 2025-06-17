5aea3c79ede2ba0e39da532c03f3b72ee65718fb
Arthur Corrêa Pacheco <pescadorlendario2@gamil.com>
2025-06-17 09:34:03 -0300

    primera versao

---
+++ NovoWEB/ativa.js
@@ -0,0 +1,10 @@
+// script.js
+function expandir() {
+    document.querySelector('.conteudo-js').style.display = 'block';
+    document.querySelector('.botao-js').style.height = '200px';
+}
+
+function recolher() {
+    document.querySelector('.conteudo-js').style.display = 'none';
+    document.querySelector('.botao-js').style.height = '50px';
+}
