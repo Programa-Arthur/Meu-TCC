7cb0aad64900caa7e3ebb9ecb3aa82f8e8908795
Kaue Santana Soares <aluno@lab32-pc01.sombrio.ifc.edu.br>
2025-07-15 09:15:04 -0300

    botoes aredondaos

--- NovoWEB/ativa.js
+++ NovoWEB/ativa.js
@@ -1,11 +1,18 @@
 // script.js
-function expandir() {
-    document.querySelector('.conteudo-js').style.display = 'block';
-    document.querySelector('.botao-js').style.height = '200px';
+function expandir(botao) {
+    botao.querySelector('.conteudo-js').style.display = 'block';
+    botao.style.height = '200px';
 }
 
-function recolher() {
-    document.querySelector('.conteudo-js').style.display = 'none';
-    document.querySelector('.botao-js').style.height = '50px';
+function recolher(botao) {
+    botao.querySelector('.conteudo-js').style.display = 'none';
+    botao.style.height = '50px';
 }
 
+// Adiciona eventos aos botões após o carregamento da página
+window.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.botao-js').forEach(botao => {
+        botao.addEventListener('mouseenter', () => expandir(botao));
+        botao.addEventListener('mouseleave', () => recolher(botao));
+    });
+});
